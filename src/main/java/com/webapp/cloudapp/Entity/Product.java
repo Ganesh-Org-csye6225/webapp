c2bacb2172e9073889ea283725d8dbd519cc7643
Pavan Ganesh <vasa.sr@northeastern.edu>
2023-02-08 20:35:54 -0500

    db schema

--- src/main/java/com/webapp/cloudapp/Entity/Product.java
+++ src/main/java/com/webapp/cloudapp/Entity/Product.java
@@ -10,7 +10,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "product", schema = "temp")
+@Table(name = "product", schema = "public")
 public class Product {
 
     @Id
