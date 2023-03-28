52078e5194d60c9a496215db6101fff53379dc83
Pavan Ganesh <vasa.sr@northeastern.edu>
2023-03-27 21:45:50 -0400

    metrics and logs

--- src/main/java/com/webapp/cloudapp/Controllers/ImageController.java
+++ src/main/java/com/webapp/cloudapp/Controllers/ImageController.java
@@ -3,8 +3,9 @@ package com.webapp.cloudapp.Controllers;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,15 +13,14 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.timgroup.statsd.StatsDClient;
 import com.webapp.cloudapp.Services.ImageService;
 
-import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 public class ImageController {
@@ -28,16 +28,24 @@ public class ImageController {
 	@Autowired
 	ImageService imageService;
 
+	@Autowired
+    private StatsDClient statsDClient;
+
+	Logger logger = LoggerFactory.getLogger(ImageController.class);
+
 	@PostMapping(value = "/v1/product/{productId}/image", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE,
 			MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<?> addImage(@RequestParam("file") MultipartFile multipartFile,
 			@PathVariable String productId, NativeWebRequest nativeWebRequest) {
+		statsDClient.incrementCounter("post.imageRequest.count");
+		logger.info("ImageController: Adding an image for a product");
 		try {
 			return imageService.createImage(multipartFile, productId, nativeWebRequest);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			Map<String, Object> resMap = new HashMap<>();
 			resMap.put("msg", "Please enter valid data");
+			logger.error("ImageController: Error adding an image for a product", e.getLocalizedMessage());
 			return new ResponseEntity<>(resMap, HttpStatusCode.valueOf(400));
 		}
 	}
@@ -45,24 +53,30 @@ public class ImageController {
 	@GetMapping("/v1/product/{productId}/image/{imageId}")
 	public ResponseEntity<?> getImage(@PathVariable String productId, @PathVariable String imageId,
     NativeWebRequest nativeWebRequest) {
+		statsDClient.incrementCounter("get.imageRequest.count");
+		logger.info("ImageController: Fetching the image of a product");
 		try {
 			return imageService.getImage(imageId, productId, nativeWebRequest);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			Map<String, Object> resMap = new HashMap<>();
 			resMap.put("msg", "Please enter valid data");
+			logger.error("ImageController: Error fetching the image of a product",e.getLocalizedMessage());
 			return new ResponseEntity<>(resMap, HttpStatusCode.valueOf(400));
 		}
 	}
 
 	@GetMapping("/v1/product/{productId}/image")
 	public ResponseEntity<?> getAllImages(@PathVariable String productId,NativeWebRequest nativeWebRequest) {
+		statsDClient.incrementCounter("getAll.imageRequest.count");
+		logger.info("ImageController: Fetching all the images of a product");
 		try {
 			return imageService.getAllImage(productId, nativeWebRequest);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			Map<String, Object> resMap = new HashMap<>();
 			resMap.put("msg", "Please enter valid data");
+			logger.error("ImageController: Error fetching the images of a product",e.getLocalizedMessage());
 			return new ResponseEntity<>(resMap, HttpStatusCode.valueOf(400));
 		}
 	}
@@ -70,12 +84,15 @@ public class ImageController {
 	@DeleteMapping("/v1/product/{productId}/image/{imageId}")
 	public ResponseEntity<?> deleteImages(@PathVariable String productId, @PathVariable String imageId,
     NativeWebRequest nativeWebRequest) {
+		statsDClient.incrementCounter("delete.imageRequest.count");
+		logger.info("ImageController: Deleting the image of a product");
 		try {
 			return imageService.deleteImage(imageId, productId, nativeWebRequest);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			Map<String, Object> resMap = new HashMap<>();
 			resMap.put("msg", "Please enter valid data");
+			logger.error("ImageController: Error deleting the image of a product");
 			return new ResponseEntity<>(resMap, HttpStatusCode.valueOf(400));
 		}
 	}
