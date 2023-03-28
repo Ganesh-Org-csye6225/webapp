b7c6d7f516b93f64f363d8c5bec175fa145f2ee2
Srinivasa Pavan Ganesh Vasa <113486368+ganeshvasa5@users.noreply.github.com>
2023-03-28 12:32:51 -0400

    Update ImageController.java

--- src/main/java/com/webapp/cloudapp/Controllers/ImageController.java
+++ src/main/java/com/webapp/cloudapp/Controllers/ImageController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.timgroup.statsd.StatsDClient;
 import com.webapp.cloudapp.Services.ImageService;
 
